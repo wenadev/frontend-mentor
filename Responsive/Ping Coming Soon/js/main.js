93bbcfb25e185159b03e47a9eeaafa80f8b3d82f
Mawuena <53694230+wenadev@users.noreply.github.com>
2020-12-16 00:01:12 +0100

    initial commit

---
+++ Responsive/Ping Coming Soon/js/main.js
@@ -0,0 +1,25 @@
+let submitButton = document.querySelector("#submit");
+let emailField = document.querySelector("#email");
+let emailText = document.querySelector("#error");
+let emailIcon = document.querySelector("#error-icon");
+
+
+submitButton.addEventListener("click", (event)=>{
+    event.preventDefault();
+        let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+        if((emailField.value.match(mailformat)) == null || emailField.value == ""){
+            console.log(emailField.classList)
+            //error text warning
+            emailText.classList.add("error-show");
+            emailField.classList.add("error-border");
+            console.log(emailField.classList)
+        }
+
+        else{
+            emailText.classList.remove("error-show");
+            emailField.classList.remove("error-border");
+            emailField.value="";
+            alert("Email succesfully registered")
+        }
+})
